834f819d17ea73c1b3c188fec3cc50666dd80e13
noahbuscher <me@noahbuscher.com>
2018-12-09 00:04:08 -0600

    Added more home page information

--- client/src/modules/App/App.js
+++ client/src/modules/App/App.js
@@ -28,16 +28,59 @@ class App extends Component {
           </div>
         </section>
 
-        <section className="cf ph3 ph5-ns pb5 bg-white black-70">
-          <div className="pa3">
-            <h1 className="mt0 f5 f3-ns pt5">Why?</h1>
-            <p className="mv0 f5 pb4 lh-copy measure">
-              Greenroom picks up where online review sites leave off, providing
-              an exhaustive, unbiased listing of legal cannabis dispensaries through
-              crowdsourcing and curation.
-            </p>
+        <div className="tl bt b--black-10 pa3 pa5-ns bg-white black-70" id="principles">
+          <div className="mw9 center">
+            <h1 className="f5 ttu tracked fw6">Features</h1>
+            <section className="lh-copy">
+              <div className="cf">
+                <article className="fl pv2 w-100 w-third-l pr4-l">
+                  <h2 className="f5 f4-ns fw6 mb0"> Lead Tracking</h2>
+                  <p className="f6 f5-ns measure lh-copy mt0">
+                    You can track the status of your leads without even leaving
+                    the comfort of Greenroom. How easy is that?
+                  </p>
+                </article>
+                <article className="pv2 fl w-100 w-third-l ph3-l">
+                  <h2 className="f5 f4-ns fw6 mb0">CSV Upload</h2>
+                  <p className="f6 f5-ns measure lh-copy mt0">
+                    Have a lot of dispensaries to import? No worries. We provide
+                    a simple CSV upload schema to get you going faster.
+                  </p>
+                </article>
+                <article className="pv2 fl w-100 w-third-l pl4-l">
+                  <h2 className="f5 f4-ns  fw6 mb0">Research</h2>
+                  <p className="f6 f5-ns measure lh-copy mt0">
+                    We integrate with Google Maps to help you find phone numbers
+                    and even hours of operation just from an address.
+                  </p>
+                </article>
+              </div>
+              <div className="cf w-100">
+                <article className="pv2 fl w-100 w-third-l pl0 pr4-l">
+                  <h2 className="f5 f4-ns fw6 mb0">Mobile</h2>
+                  <p className="f6 f5-ns measure lh-copy mt0">
+                    Need to track leads in the field? No problem - Greenroom
+                    is mobile-ready to help you take on the world.
+                  </p>
+                </article>
+                <article className="pv2 fl w-100 w-third-l ph3-l">
+                  <h2 className="f5 f4-ns  fw6 mb0">Advanced Filters</h2>
+                  <p className="f6 f5-ns measure lh-copy mt0">
+                    You can filter stores by city, state, and status, allowing
+                    you to find what you are looking for.
+                  </p>
+                </article>
+                <article className="pv2 fl w-100 w-third-l pl4-l">
+                  <h2 className="f5 f4-ns fw6 mb0">Open Source</h2>
+                  <p className="f6 f5-ns measure lh-copy mt0">
+                    Built using modern web tools like React, MongoDB, and Node,
+                    creating new featres is as easy as making a pull request.
+                  </p>
+                </article>
+              </div>
+            </section>
           </div>
-        </section>
+        </div>
       </div>
     );
   }

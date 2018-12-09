d5297319bd5f7a26843b352bd44eee631e0269e6
noahbuscher <me@noahbuscher.com>
2018-12-08 19:39:05 -0600

    Submit page structure

--- client/src/modules/App/App.js
+++ client/src/modules/App/App.js
@@ -14,16 +14,28 @@ class App extends Component {
       <div>
         <Header />
         <section className="cf ph3 ph5-ns pb5 bg-green black-70">
-          <h1 className="f4 pt3 f1-ns fw6 mb2">Greenroom</h1>
-          <h2 className="f6 fw2 pb3 ttu tracked">For Flowhub</h2>
-          <p className="mv0 f5 pb4 lh-copy measure">
-            Greenroom picks up where online review sites leave off, providing
-            an exhaustive, unbiased listing of legal cannabis dispensaries through
-            crowdsourcing and curation.
-          </p>
-          <a href="/explore" className="f6 f5-ns fw6 dib ba b--black-20 bg-dark-green white ph3 ph4-ns pv2 pv3-ns br2 grow no-underline">
-            Explore Stores
-          </a>
+          <div className="pa3">
+            <h1 className="f4 pt3 f1-ns fw6 mb2">Greenroom</h1>
+            <h2 className="f6 fw2 pb3 ttu tracked">For Flowhub</h2>
+            <p className="mv0 f5 pb4 lh-copy measure">
+              Greenroom picks up where online review sites leave off, providing
+              an exhaustive, unbiased listing of legal cannabis dispensaries through
+              crowdsourcing and curation.
+            </p>
+            <a href="/explore" className="f6 f5-ns fw6 dib ba b--black-20 bg-dark-green white ph3 ph4-ns pv2 pv3-ns br2 grow no-underline">
+              Explore Stores
+            </a>
+          </div>
+        </section>
+        <section className="cf ph3 ph5-ns pb5 bg-white black-70">
+          <div className="pa3">
+            <h1 className="mt0 f5 f3-ns pt5">Why?</h1>
+            <p className="mv0 f5 pb4 lh-copy measure">
+              Greenroom picks up where online review sites leave off, providing
+              an exhaustive, unbiased listing of legal cannabis dispensaries through
+              crowdsourcing and curation.
+            </p>
+          </div>
         </section>
       </div>
     );
--- client/src/modules/Store/pages/StoreCreatePage/StoreCreatePage.js
+++ client/src/modules/Store/pages/StoreCreatePage/StoreCreatePage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './StoreCreatePage.css';
+import Header from '../../../../components/Header/Header';
 
 class StoreCreatePage extends Component {
   constructor(props) {
@@ -41,37 +41,75 @@ class StoreCreatePage extends Component {
     } = this.state;
 
     return (
-      <div className="container">
-        <form>
-          <input
-            type="text"
-            name="name"
-            placeholder="Store name..."
-            value={name}
-            onChange={this.handleChangeText}
-          />
-          <input
-            type="text"
-            name="street"
-            placeholder="Store address..."
-            value={street}
-            onChange={this.handleChangeText}
-          />
-          <input
-            type="text"
-            name="city"
-            placeholder="Store city..."
-            value={city}
-            onChange={this.handleChangeText}
-          />
-          <input
-            type="text"
-            name="state"
-            placeholder="Store state..."
-            value={state}
-            onChange={this.handleChangeText}
-          />
-        </form>
+      <div>
+        <Header />
+        <section className="cf ph3 ph5-ns pb3 bg-green black-70">
+          <h2 className="f3 f1-ns fw6 mb2 lh-copy">Submit</h2>
+          <p className="mv0 f5 pb4 lh-copy measure">
+            Add a new dispensary to the database.
+          </p>
+        </section>
+
+        <section className="cf ph5-ns pb5 pv4 bg-white black-70">
+          <form>
+            <div className="measure">
+              <label for="name" className="f6 b db mb2">Store Name</label>
+              <input
+                id="name"
+                className="input-reset ba b--black-20 pa2 mb2 db w-100"
+                type="text"
+                aria-describedby="name-desc"
+                value={name}
+                onChange={this.handleChangeText}
+              />
+              <small id="name-desc" className="f6 black-60 db mb2">Ex: 303 Dispensary</small>
+            </div>
+
+            <div className="measure">
+              <label for="address" className="f6 b db mb2">Address</label>
+              <input
+                id="street"
+                className="input-reset ba b--black-20 pa2 mb2 db w-100"
+                type="text"
+                aria-describedby="address-desc"
+                value={street}
+                onChange={this.handleChangeText}
+              />
+              <small id="address-desc" className="f6 black-60 db mb2">Ex. 417 Wewatta Street</small>
+            </div>
+
+            <div className="measure">
+              <label for="city" className="f6 b db mb2">City</label>
+              <input
+                id="city"
+                className="input-reset ba b--black-20 pa2 mb2 db w-100"
+                type="text"
+                aria-describedby="city-desc"
+                value={city}
+                onChange={this.handleChangeText}
+              />
+              <small id="city-desc" className="f6 black-60 db mb2">Ex: Denver</small>
+            </div>
+
+            <div className="measure">
+              <label for="state" className="f6 b db mb2">State</label>
+              <input
+                id="state"
+                className="input-reset ba b--black-20 pa2 mb2 db w-100"
+                type="text"
+                aria-describedby="state-desc"
+                value={state}
+                onChange={this.handleChangeText}
+              />
+              <small id="state-desc" className="f6 black-60 db mb2">Ex: Colorado</small>
+            </div>
+            <div className="measure">
+              <button className="f6 f5-ns fw6 dib ba b--black-20 bg-dark-green white ph3 ph4-ns pv2 pv3-ns br2 grow no-underline">
+                Submit
+              </button>
+            </div>
+          </form>
+        </section>
       </div>
     );
   }
